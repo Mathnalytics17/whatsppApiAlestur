27ff5b0ec45ef596857ae8c205688c3efb006109
Luis Molina <luisjose0317@gmail.com>
2026-04-24 18:46:31 -0500

    dockerizacion6

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
 import os
 
-# Configuración de conexión a la base de datos PostgreSQL
-DB_USER = "alestur_user"          # usuario que creaste en PostgreSQL
-DB_PASS = "tu_clave_segura"     # contraseña que le diste (puedes dejarla simple mientras pruebas)
-DB_NAME = "superpassword"        # nombre de la base creada
-DB_HOST = "localhost"           # servidor local
-DB_PORT = "5432"                # puerto por defecto de PostgreSQL
+DB_USER = "alestur_user"
+DB_PASS = "superpassword"
+DB_NAME = "alestur_db"
+DB_HOST = "db"
+DB_PORT = "5432"
 
 SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# Clave secreta de Flask (para sesiones, formularios, etc.)
-SECRET_KEY = "supersecretkey"
+SECRET_KEY = "superpassword"
